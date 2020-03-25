99a40525dacefc5b63b2b1174acd235909e04a07
ParkerBacall <pbacall2@gmail.com>
2020-03-24 19:17:54 -0600

    fix errors

--- index.py
+++ index.py
@@ -1,13 +1,15 @@
-is_game_over: false
+is_game_over= False
 p_x_pos = 0
 e_x_pos = 3
 end_x_pos = 10
 
 while not is_game_over:
-    if p_x_pos == e_x_pos
+    if p_x_pos == e_x_pos:
         is_game_over = True
-    elif p_x_pos >= end_x_pos
+        print("you lose!")
+    elif p_x_pos >= end_x_pos:
         is_game_over = False
+        print("you win!")
     else:
         p_x_pos += 2
         e_x_pos += 1
